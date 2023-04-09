34025379ca6738ef36262b2c15de8a6db2b45e8c
Shivang1102 <shivanggupta1102@gmail.com>
2023-04-09 23:49:42 +0530

    final

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,12 +3,20 @@ import React from 'react';
 import classes from './Movie.module.css';
 
 const Movie = (props) => {
+
+  const deleteHandler=()=>{
+    
+  }
   return (
+    <>
     <li className={classes.movie}>
       <h2>{props.title}</h2>
       <h3>{props.releaseDate}</h3>
       <p>{props.openingText}</p>
+     
     </li>
+     <button className={'btn btn-warning'} onClick={deleteHandler}>Delete </button>
+     </>
   );
 };
 
